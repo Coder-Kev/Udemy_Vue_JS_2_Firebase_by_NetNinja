7ab6f3b8d351c00eaa4b21e50cf4c65c8c305ff6
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2018-11-11 16:09:21 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar /> <AllFriends /> <OnlineFriends />
+  </div>
+</template>
+
+<script>
+import Navbar_Vue from "./Navbar.vue";
+import AllFriends_Vue from "./AllFriends.vue";
+import OnlineFriends_Vue from "./OnlineFriends.vue";
+
+export default {
+  name: "app",
+  components: {
+    Navbar: Navbar_Vue,
+    AllFriends: AllFriends_Vue,
+    OnlineFriends: OnlineFriends_Vue
+  },
+  data() {
+    return {
+      title: "My first Vue App"
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
