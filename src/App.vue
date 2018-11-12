aab264bfd2e473feae3448a788761762f1a9b32a
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2018-11-11 17:24:50 -0800

    Sect03-Lect25: Create Global Vue Filter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    My Progress on course from Udemy on Vue JS 2 with Firebase by The Net Ninja
+    <hr />
     <h1>{{ title }}</h1>
     <Navbar /> <Blogs />
   </div>
--- src/Blogs.vue
+++ src/Blogs.vue
@@ -4,7 +4,7 @@
     <hr />
     <div v-for="post in posts" :key="post.id">
       <h3>{{ post.title }}</h3>
-      <p>{{ post.body }}</p>
+      <p>{{ post.body | snippet }}</p>
     </div>
   </div>
 </template>
