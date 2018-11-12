2952ee86cbbb75883fff6be1ed87802a996f9852
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2018-11-11 17:47:20 -0800

    Sect03-Lect26: Computed Properties (Custom Search Box)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     My Progress on course from Udemy on Vue JS 2 with Firebase by The Net Ninja
+    <br />
+    SECTION 3, LECTURE 26: Computed Properties (Custom Search Box) <br />
+    Set Blogs.vue.active = true to see data THIS SECTION IS
     <hr />
     <h1>{{ title }}</h1>
     <Navbar /> <Blogs />
--- src/Blogs.vue
+++ src/Blogs.vue
@@ -1,9 +1,19 @@
+// Added custom [active] flag to stop it from fetching JSON while editting in
+CodeSandbox.io
+
+<script>
+export default {};
+</script>
+
+<style></style>
 <template>
   <div class="blogs">
-    <h2>{{ blogTitle }}</h2>
+    <h2>{{ blogTitle }} - Active = ({{ active }})</h2>
+    SEARCH:<input type="text" v-model="searchTerm" />
     <hr />
-    <div v-for="post in posts" :key="post.id">
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
+
       <p>{{ post.body | snippet }}</p>
     </div>
   </div>
@@ -17,27 +27,34 @@ export default {
   data() {
     return {
       blogTitle: "Blogs",
+      active: false,
+      searchTerm: "",
       posts: []
     };
   },
-  methods: {
-    changeTitle() {
-      this.blogTitle = "Amazing Blog Title";
+  methods: {},
+  computed: {
+    filteredPosts() {
+      return this.posts.filter(post => {
+        return post.title.match(this.searchTerm);
+      });
     }
   },
   // Lifecycle hooks
   created() {
     // get sample JSON from https://jsonplaceholder.typicode.com/
     // axios returns a promise
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {
-        console.log(response);
-        this.posts = response.data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    if (this.active) {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+          console.log(response);
+          this.posts = response.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
